1eff02a037defc46d4cef0748c3b415f9a76182f
Rodrigo Linux <67449702+rodrigolinuxx@users.noreply.github.com>
2021-04-06 01:29:15 -0300

    Update edge_debian.sh

--- edge_debian.sh
+++ edge_debian.sh
@@ -8,7 +8,7 @@ install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 echo 'deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main' > /etc/apt/sources.list.d/microsoft-edge-dev.list
 rm microsoft.gpg
 apt update
-apt install microsoft-edge-dev
+apt install microsoft-edge-dev"
 EOF
 sleep 1
 echo
