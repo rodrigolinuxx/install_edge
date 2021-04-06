c2e2a0aa275577a8850871db4ac68d275dd200b4
Rodrigo Linux <67449702+rodrigolinuxx@users.noreply.github.com>
2021-04-06 00:42:17 -0300

    Add files via upload

---
+++ edge.sh
@@ -0,0 +1,15 @@
+echo
+echo $'--------------\nMicrosoft Edge...'
+echo '-----------------'
+echo
+sleep 2
+sudo sh -c << EOF "curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+echo 'deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main' > /etc/apt/sources.list.d/microsoft-edge-dev.list
+rm microsoft.gpg
+apt update
+apt install microsoft-edge-dev
+#apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 2E30EE6263DE9CD4"
+EOF
+sleep 1
+echo
