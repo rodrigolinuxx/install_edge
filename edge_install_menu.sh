9006797e5ba6bafa67593f9e5972fb022f47edeb
Rodrigo Costa <67449702+rodrigolinux@users.noreply.github.com>
2021-06-29 09:38:05 -0300

    Update edge_install_menu.sh

--- edge_install_menu.sh
+++ edge_install_menu.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 #
-######################################################
-# Autor:     Rodrigo Costa                           #
-# Contato:   rodrigo.costa@outlook.it                #
-# Link:      https://github.com/rodrigolinuxx        #
-######################################################
+##########################################################################
+# Autor:     Rodrigo Costa                           			 #
+# Contato:   rodrigo.costa@outlook.it                			 #
+# Link:      https://github.com/rodrigolinux/install_edge		 #
+# Data:      06/04/2021                              			 #
+# Licença:   GPLv3                                   			 #
+# Sobre:     Script que ao ser executado exibe uma janela (em Dialog)    #
+#            solicitando qual formato do navegador Edge deseja instalar  #
+##########################################################################
 #
 if [[ $EUID -ne 0 ]]; then
    echo "Deve ser executado como root." 
@@ -39,13 +43,13 @@ case $OPT in
 			echo 'deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main' > /etc/apt/sources.list.d/microsoft-edge-dev.list
 			rm microsoft.gpg
 			apt update
-			apt install microsoft-edge-dev
+			apt install microsoft-edge-dev -y
             ;;
         2)
             echo "Instalando .rpm"
 			rpm --import https://packages.microsoft.com/keys/microsoft.asc
 			dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
 			mv /etc/yum.repos.d/packages.microsoft.com_yumrepos_edge.repo /etc/yum.repos.d/microsoft-edge-dev.repo
-			dnf install microsoft-edge-dev
+			dnf install microsoft-edge-dev -y
             ;;
 esac
